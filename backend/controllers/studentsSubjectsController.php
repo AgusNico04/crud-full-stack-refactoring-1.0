aba666c03386f5d166fc00ce99b357ccf51fe807
Starter-art <luquitaslanari30@gmail.com>
2025-11-18 06:40:39 +0000

    AsigPorMateria

--- backend/controllers/studentsSubjectsController.php
+++ backend/controllers/studentsSubjectsController.php
@@ -71,4 +71,17 @@ function handleDelete($conn)
         echo json_encode(["error" => "No se pudo eliminar"]);
     }
 }
+
+function getStudentsBySubject($conn, $subject_id)
+{
+    $sql = "SELECT id, fullname FROM students AS s
+            JOIN students_subjects AS ss ON s.id = ss.id
+            WHERE ss.subject_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute;
+    $result = $stmt->get_result();
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
 ?>
