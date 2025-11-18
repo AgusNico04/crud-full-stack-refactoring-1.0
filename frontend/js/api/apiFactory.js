9a86267cc8f1d3a09b703a6e6dae2ded25254dd1
RomanStele <stelleroman@gmail.com>
2025-11-18 11:34:22 -0300

    Actualizacion de funcion createAPI, en especifico senJSON para enviar tipo de error y archivos students para detectar mail duplicado

--- frontend/js/api/apiFactory.js
+++ frontend/js/api/apiFactory.js
@@ -14,19 +14,31 @@ export function createAPI(moduleName, config = {})
 
     async function sendJSON(method, data) 
     {
-        const res = await fetch(API_URL,
-        {
+        const res = await fetch(API_URL, {
             method,
-            headers: { 'Content-Type': 'application/json' },
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
         });
 
-        if (!res.ok) 
-        {
-            const errorData = await res.json();
-            throw new Error(errorData.error || `Error en ${method}`);
+        let result;//variable para almacenar el resultado parseado
+
+        try {
+            result = await res.json();  // intenta parsear JSON
+        } 
+        catch (e) {
+        // Si falla, entonces la respuesta NO era JSON
+            throw {
+                error: "invalid_json",
+                message: "El servidor devolvió un formato no válido",
+                raw: await res.text()
+            };
         }
-        return await res.json();
+
+        if (!res.ok) {//si la respuesta no es OK (código 200-299)
+            throw result; // Tiramos el JSON
+        }
+
+        return result;//devolvemos el resultado parseado
     }
 
     return {
